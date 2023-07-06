f6aace7bfa50c91acfc89e70405d72cd478b59e0
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-06 18:20:15 -0300

    :sparkles: feat: created router component

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import { Series } from './pages/Series';
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>home</h1>} />
+      <Route path="/series" element={<Series />} />
+    </Routes>
+  );
+}
