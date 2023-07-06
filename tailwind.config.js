87a082d077ed4c623e34bc86229892974c9d0846
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-05 21:18:57 -0300

    :sparkles: feat: roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+
+      colors: {
+        red: {
+          500: '#F75A68',
+        },
+
+        green: {
+          300: '#00B37E',
+          400: '#00875F',
+        },
+
+        blue: {
+          500: '#81d8f7',
+        },
+
+        gray: {
+          500: '#323238',
+          600: '#23252b',
+          800: '#141519',
+          900: '#17171A',
+        },
+
+        orange: {
+          400: '#FBA94C',
+          600: '#f47521',
+        },
+      },
+    },
   },
   plugins: [],
 };
