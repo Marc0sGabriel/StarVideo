e7ba8078298c1d9b07a0cc777a572cf2da0bd9ea
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-06 19:21:18 -0300

    :lipstick:  change: footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <>
-      <footer className="w-full flex py-4 justify-center text-zinc-400 border-gray-400 border-t">
+      <footer className="w-full flex py-2 justify-center text-zinc-400 border-gray-400 border-t">
         <span>✦ Desenvolvido por Marcos Gabriel</span>
       </footer>
     </>
