768614a7b5a56b8345b9a8840421cb26285ccec3
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-06 09:45:43 -0300

    :sparkles: feat: getting data from cms

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -1,19 +1,42 @@
-export function Episodes() {
+import { CheckCircle, Lock } from '@phosphor-icons/react';
+
+interface EpisodesProps {
+  title: string;
+  slug: string;
+  avaliableAt: string;
+  type: 'free' | 'premium';
+}
+
+export function Episodes(props: EpisodesProps) {
+  const isEpisodeAvailableFree = props.type === 'free';
+
   return (
     <a href="#">
       <span className="text-gray-300">Terça • 22 de junho • 19h00</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium">
-            Episodio Liberado
-          </span>
+          {isEpisodeAvailableFree ? (
+            <span className="text-sm flex items-center gap-2 text-blue-500 font-medium">
+              <CheckCircle size={20} weight="bold" />
+              Episodio Liberado
+            </span>
+          ) : (
+            <span
+              title="Em breve disponivel para usuarios comuns"
+              className="text-sm flex items-center gap-2 text-orange-400 font-medium"
+            >
+              <Lock size={20} weight="bold" />
+              Em Breve
+            </span>
+          )}
+
           <span className="text-xs text-orange-500 px-2 rounded py-[0.125rem] border-2 border-orange-500 font-bold">
-            Novo ✦
+            {props.type === 'free' ? 'Novo ✦' : 'Premium 🜲'}
           </span>
         </header>
 
-        <strong>Operaçao Strix</strong>
+        <strong className="text-zinc-100 mt-5 block">{props.title}</strong>
       </div>
     </a>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,33 @@
+import { gql, useQuery } from '@apollo/client';
 import { Episodes } from './Episodes';
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    episodes(orderBy: publishedAt_ASC, stage: PUBLISHED) {
+      id
+      slug
+      title
+      availableAt
+      episodeType
+    }
+  }
+`;
+
+interface GetEpisodesQueryResponse {
+  episodes: {
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    episodeType: 'free' | 'premium';
+  }[];
+}
+
 export function Sidebar() {
+  const { data } = useQuery<GetEpisodesQueryResponse>(GET_LESSONS_QUERY);
+
+  console.log(data);
+
   return (
     <aside className="w-[348px] bg-gray-900 p-6 border-l border-zinc-800">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-zinc-700 block">
@@ -8,12 +35,17 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        <Episodes />
-        <Episodes />
-        <Episodes />
-        <Episodes />
-        <Episodes />
-        <Episodes />
+        {data?.episodes.map((episode) => {
+          return (
+            <Episodes
+              key={episode.id}
+              title={episode.title}
+              slug={episode.slug}
+              avaliableAt={episode.availableAt}
+              type={episode.episodeType}
+            />
+          );
+        })}
       </div>
     </aside>
   );
