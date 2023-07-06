16a6b4152e8f56a7d153ea90e0bc4bd26b307b44
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-06 18:19:45 -0300

    :sparkles: feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Home } from './pages/Home';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './lib/apollo';
+import { Router } from './Router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </>
   );
 }
