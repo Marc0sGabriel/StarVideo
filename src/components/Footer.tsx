a210e571342fdc6a4b830e9ab90400c8572fd154
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-06 15:14:40 -0300

    :sparkles: feat: initial footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <>
+      <footer className="w-full flex py-4 justify-center text-zinc-400 border-gray-400 border-t">
+        <span>✦ Desenvolvido por Marcos Gabriel</span>
+      </footer>
+    </>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
+import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 import { Video } from '../components/Video';
 
 export function Home() {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main className='flex flex-1'>
+      <main className="flex flex-1">
         <Video />
         <Sidebar />
       </main>
+      <Footer />
     </div>
   );
 }
