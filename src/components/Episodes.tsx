983e8bc99f78a66ab1899c18f1f303c3c0f4d40a
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-07 09:23:53 -0300

    :sparkles: feat: styled active episode

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -1,7 +1,8 @@
 import { CheckCircle, Lock } from '@phosphor-icons/react';
 import { isPast, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface EpisodesProps {
   title: string;
@@ -11,6 +12,8 @@ interface EpisodesProps {
 }
 
 export function Episodes(props: EpisodesProps) {
+  const { slug } = useParams<{ slug: string }>();
+
   const isEpisodeAvailableFree = isPast(props.avaliableAt);
   const availableDateFormatted = format(
     props.avaliableAt,
@@ -20,20 +23,36 @@ export function Episodes(props: EpisodesProps) {
     }
   );
 
+  const isActiveEpisode = slug === props.slug;
+
   return (
     <Link to={`/series/episode/${props.slug}`} className="group">
       <span className="text-zinc-500">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-orange-400">
+      <div
+        className={classNames(
+          'rounded border border-gray-500 p-4 mt-2 group-hover:border-orange-400',
+          {
+            'bg-orange-500': isActiveEpisode,
+          }
+        )}
+      >
         <header className="flex items-center justify-between">
           {isEpisodeAvailableFree ? (
-            <span className="text-sm flex items-center gap-2 text-blue-500 font-medium">
+            <span
+              className={classNames(
+                'text-sm flex items-center gap-2 text-blue-500 font-medium',
+                {
+                  'text-zinc-100': isActiveEpisode,
+                }
+              )}
+            >
               <CheckCircle size={20} weight="bold" />
               Episodio Liberado
             </span>
           ) : (
             <span
-              title="Em breve disponivel para usuarios premium"
+              title="Em breve disponivel para usfuarios premium"
               className="text-sm flex items-center gap-2 text-orange-400 font-medium"
             >
               <Lock size={20} weight="bold" />
@@ -41,7 +60,14 @@ export function Episodes(props: EpisodesProps) {
             </span>
           )}
 
-          <span className="text-xs text-orange-500 px-2 rounded py-[0.125rem] border-2 border-orange-500 font-bold">
+          <span
+            className={classNames(
+              'text-xs text-orange-500 px-2 rounded py-[0.125rem] border-2 border-orange-500 font-bold',
+              {
+                'text-zinc-200 border-zinc-200': isActiveEpisode,
+              }
+            )}
+          >
             {props.type === 'free' ? 'Novo ✦' : 'Premium 🜲'}
           </span>
         </header>
