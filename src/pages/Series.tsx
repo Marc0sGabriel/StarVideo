749e7a9c48842f907979d0bf5fbdc713a3ce0452
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-07 09:24:35 -0300

    :bug: fix: 404 route

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -11,7 +11,7 @@ export function Series() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video episodeSlug={slug} />
+        {slug ? <Video episodeSlug={slug} /> : <div className="flex-1" />}
         <Sidebar />
       </main>
       <Footer />
