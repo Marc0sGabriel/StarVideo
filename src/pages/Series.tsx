914bc21f86e8ac10a6fbe8debdc365501abec325
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-06 21:13:45 -0300

    :sparkles: feat: dynamic videos

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -1,14 +1,17 @@
+import { useParams } from 'react-router-dom';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 import { Video } from '../components/Video';
 
 export function Series() {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        <Video episodeSlug={slug} />
         <Sidebar />
       </main>
       <Footer />
