d05bd6fbb74cdb7a3c9bc8d9cf7b829f54247d4d
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-09 10:10:08 -0300

    :sparkles: feat: HomePage

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import { Series } from './pages/Series';
+import { Serie } from './pages/Serie';
 import { Subscribe } from './pages/Subscribe';
 import { NotFound } from './components/NotFound';
+import { Home } from './pages/home/Home';
 
 export function Router() {
   return (
     <Routes>
       <Route path="*" element={<NotFound />} />
       <Route path="/" element={<Subscribe />} />
-      <Route path="/series/episode/:slug" element={<Series />} />
-      <Route path="/series" element={<Series />} />
+      <Route path="/series" element={<Home />} />
+      <Route path="/series/episode/:slug" element={<Serie />} />
     </Routes>
   );
 }
