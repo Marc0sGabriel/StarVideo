64d27a789385814cd7a3bc9552681b381749db4e
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-09 10:15:58 -0300

    :sparkles: feat: Series Card Component

---
+++ src/pages/home/components/SeriesCard.tsx
@@ -0,0 +1,14 @@
+export function SeriesCard() {
+  return (
+    <a href="/series/episode/operacao-strix">
+      <img
+        src="https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/689e2efcf9f192ba6c0f7a538ee99027.jpe"
+        alt=""
+        height={300}
+        width={120}
+      />
+      <h2 className="font-bold text-sm mt-2">SPY x FAMILY</h2>
+      <span className="font-medium text-sm text-starBlue-700">Leg | Dub</span>
+    </a>
+  );
+}
