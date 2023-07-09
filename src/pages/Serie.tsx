aaa1ddf7fd43ab16fc1b5fe3ff29e83f78fd1b1f
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-09 10:13:25 -0300

    :sparkles: feat: Serie Component

---
+++ src/pages/Serie.tsx
@@ -0,0 +1,24 @@
+import { useParams } from 'react-router-dom';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
+import { Video } from '../components/Video';
+
+export function Serie() {
+  const { slug } = useParams<{ slug: string }>();
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-col flex-1 lg:flex-row">
+        {slug ? (
+          <Video episodeSlug={slug} />
+        ) : (
+          <div className="flex-1 bg-zinc-900" />
+        )}
+        <Sidebar />
+      </main>
+      <Footer />
+    </div>
+  );
+}
