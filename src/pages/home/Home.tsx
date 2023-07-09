fd043d7bcdf23c8b87ebebe5d2e38f579796ebe0
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-09 10:14:58 -0300

    :sparkles: feat: home component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header';
+import { SeriesNavigator } from './components/SeriesNavigator';
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <SeriesNavigator />
+    </>
+  );
+}
