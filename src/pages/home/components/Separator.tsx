468a263dde53d11e65f7c9ee5a42ab793c0961b5
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-09 11:06:56 -0300

    Revert ":lipstick: feat: new color"
    This reverts commit 57eedf2453547cc079c412a2ada2e374327e77a9.

--- src/pages/home/components/Separator.tsx
+++ src/pages/home/components/Separator.tsx
@@ -1,5 +1,3 @@
 export function Separator() {
-  return (
-    <div className="w-full max-w-full mt-2 h-1 bg-gradient-to-l from-green-300 via-blue-500 to-purple-600" />
-  );
+  return <div className="w-full mt-2 h-1 bg-starBlue-400" />;
 }
