a0d816cb8a1762fea7ee09d2dbc900c758f18532
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-09 11:14:12 -0300

    :recycle: change: image width

--- src/pages/home/components/SeriesCard.tsx
+++ src/pages/home/components/SeriesCard.tsx
@@ -5,7 +5,7 @@ export function SeriesCard() {
         src="https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/689e2efcf9f192ba6c0f7a538ee99027.jpe"
         alt=""
         height={300}
-        width={120}
+        width={150}
       />
       <h2 className="font-bold text-sm mt-2">SPY x FAMILY</h2>
       <span className="font-medium text-sm text-starBlue-700">Leg | Dub</span>
