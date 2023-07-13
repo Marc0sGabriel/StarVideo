9bcaff02691516f829952e155e3e6863813127ff
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-13 19:37:52 -0300

    :sparkles: feat: getting data from CMS

--- src/pages/home/components/SeriesCard.tsx
+++ src/pages/home/components/SeriesCard.tsx
@@ -1,18 +1,65 @@
+import { gql, useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 
+const GET_SERIES_CARD_DATA = gql`
+  query SeriesCardInfo {
+    cards {
+      title
+      id
+      slug
+      infoCard {
+        text
+      }
+      cardImage {
+        url
+        width
+      }
+    }
+  }
+`;
+
+interface SeriesCardProps {
+  cards: {
+    title: string;
+    id: string;
+    slug: string;
+    cardImage: {
+      url: string;
+      width: number;
+    };
+
+    infoCard: {
+      text: string;
+    };
+  }[];
+}
+
 export function SeriesCard() {
+  const { data } = useQuery<SeriesCardProps>(GET_SERIES_CARD_DATA);
+
   return (
-    <Link to={'/series/episode/operacao-strix'}>
-      <img
-        src="https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/689e2efcf9f192ba6c0f7a538ee99027.jpe"
-        alt=""
-        height={300}
-        width={150}
-      />
-      <h2 className="font-bold text-xs sm:text-sm mt-2">SPY x FAMILY</h2>
-      <span className="font-medium text-xs sm:text-sm text-zinc-500">
-        Leg | Dub
-      </span>
-    </Link>
+    <div className="flex my-10 gap-9">
+      {data?.cards.map((card) => {
+        return (
+          <Link key={card.id} to={`/series/episode/${card.slug}`}>
+            <div className="h-[250px] w-[150px] my-4">
+              <img
+                src={card.cardImage.url}
+                className="object-cover w-full h-full"
+                alt={card.title}
+                height={300}
+                width={150}
+              />
+              <h2 className="font-bold text-xs sm:text-sm mt-2">
+                {card.title}
+              </h2>
+              <span className="font-medium text-xs sm:text-sm text-zinc-500">
+                {card.infoCard.text}
+              </span>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
   );
 }
