a6f0f685223693326f6a8ee3beb9f8ee9d1977a8
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-14 16:37:55 -0300

    :lipstick:  styles: new header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from './Logo';
 
 export function Header() {
   return (
-    <header className="border-b border-gray-400 w-full py-5 flex bg-gray-900 items-center justify-center">
+    <header className="border-b border-gray-400 w-full py-5 flex bg-zinc-950 items-center justify-center">
       <Logo />
     </header>
   );
