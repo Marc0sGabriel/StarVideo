8a9d4fcdc17e579a148b658676cb5796d2ba387f
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-16 17:08:49 -0300

    :recycle: feat: get episode by slug

--- src/pages/Serie.tsx
+++ src/pages/Serie.tsx
@@ -7,7 +7,7 @@ import { gql, useQuery } from '@apollo/client';
 
 const GET_SHOWS_ID = gql`
   query {
-    shows {
+    shows(stage: PUBLISHED) {
       id
       title
     }
@@ -27,11 +27,6 @@ export function Serie() {
 
   const ID = data?.shows.map((ep) => ep.id);
 
-  const isActiveShow =
-    slug === 's1-ep1-loid-wants-yor-to-be-his-real-wife'
-      ? 'cljyrvlsw2zi60blvqixtk5wy'
-      : 'cljw1mis346cd0blvd0wfifrx';
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -41,7 +36,8 @@ export function Serie() {
         ) : (
           <div className="flex-1 bg-zinc-900" />
         )}
-        <Sidebar episodeID={isActiveShow} />
+
+        <Sidebar slug={slug} />
       </main>
       <FooterVideoPage />
     </div>
