569fd9601f7d1f0c288b94b40f10e6bfd9b24614
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-16 17:09:14 -0300

    :lipstick: change: gradient direction

--- src/pages/home/components/Separator.tsx
+++ src/pages/home/components/Separator.tsx
@@ -1,5 +1,5 @@
 export function Separator() {
   return (
-    <div className="w-full mt-2 h-1 bg-gradient-to-r from-starBlue-600 to-cyan-300" />
+    <div className="w-full mt-2 h-1 bg-gradient-to-l from-starBlue-600 to-cyan-300" />
   );
 }
