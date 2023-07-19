5ea3ca76a8bc34f6111b7a9e949a7f8f3374c7d5
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-19 20:57:40 -0300

    :sparkles: feat: Series Card Slider

--- src/pages/home/components/SeriesCard.tsx
+++ src/pages/home/components/SeriesCard.tsx
@@ -1,5 +1,13 @@
 import { gql, useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+// import required modules
+import { Navigation, Pagination } from 'swiper/modules';
+
+import './Carousel/SeriesCarousel/seriesCarousel.css';
 
 const GET_SERIES_CARD_DATA = gql`
   query SeriesCardInfo {
@@ -38,28 +46,43 @@ export function SeriesCard() {
   const { data } = useQuery<SeriesCardProps>(GET_SERIES_CARD_DATA);
 
   return (
-    <div className="flex my-10 gap-9">
-      {data?.cards.map((card) => {
-        return (
-          <Link key={card.id} to={`/series/episode/${card.slug}`}>
-            <div className="h-[250px] w-[150px] my-4">
-              <img
-                src={card.cardImage.url}
-                className="object-cover w-full h-full"
-                alt={card.title}
-                height={300}
-                width={150}
-              />
-              <h2 className="font-bold text-xs sm:text-sm mt-2">
-                {card.title}
-              </h2>
-              <span className="font-medium text-xs sm:text-sm text-zinc-500">
-                {card.infoCard.text}
-              </span>
-            </div>
-          </Link>
-        );
-      })}
+    <div className="flex items-center h-full max-w-full overflow-hidden justify-center gap-9">
+      <Swiper
+        slidesPerView={4}
+        freeMode={true}
+        navigation={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation, Pagination]}
+        className="supimpa"
+      >
+        {data?.cards.map((card) => {
+          return (
+            <SwiperSlide key={card.id}>
+              <Link key={card.id} to={`/series/episode/${card.slug}`}>
+                <div className="h-[250px] w-[150px] my-4">
+                  <img
+                    src={card.cardImage.url}
+                    className="object-cover w-full h-full"
+                    alt={card.title}
+                    height={300}
+                    width={150}
+                  />
+                  <h2 className="font-bold text-xs sm:text-sm mt-2">
+                    {card.title}
+                  </h2>
+                  <span className="font-medium text-xs sm:text-sm text-zinc-500">
+                    {card.infoCard.text}
+                  </span>
+                </div>
+              </Link>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 }
