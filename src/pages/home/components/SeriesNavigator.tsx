e80be7e4814a453c5415816ba68f224302a35c4d
Marc0sGabriel <mgabriel4747@gmail.com>
2023-07-20 14:33:52 -0300

    :sparkles: feat: props Series wide ad banner

--- src/pages/home/components/SeriesNavigator.tsx
+++ src/pages/home/components/SeriesNavigator.tsx
@@ -1,11 +1,13 @@
 import { Carousel } from './Carousel/Carousel';
 import { Separator } from './Separator';
 import { SeriesCard } from './SeriesCard';
+import { SeriesAdWide } from './SeriesAdWide';
 
 export function SeriesNavigator() {
   return (
     <main className="max-w-full w-full">
       <Carousel />
+
       <div className="px-4 mt-28 flex mx-auto md:max-w-[800px] lg:max-w-[1100px] flex-col gap-7 items-start">
         <div className="max-w-full w-full">
           <div>
@@ -18,10 +20,17 @@ export function SeriesNavigator() {
             <Separator />
           </div>
 
-          <nav className="h-[40rem] max-w-full w-full">
+          <nav className="h-[30rem] max-w-full w-full">
             <SeriesCard />
           </nav>
         </div>
+
+        <SeriesAdWide
+          primary={'from-purple-600'}
+          secondary={'to-red-500'}
+          btnColor={'bg-purple-600'}
+          imageUrl="https://sm.ign.com/t/ign_pl/screenshot/default/john-wick-3-review_z4q4.1280.jpg"
+        />
 
         <div className="max-w-full w-full">
           <div className="mt-10">
@@ -34,10 +43,16 @@ export function SeriesNavigator() {
             <Separator />
           </div>
 
-          <nav className="h-[40rem] max-w-full w-full">
+          <nav className="h-[30rem] max-w-full w-full">
             <SeriesCard />
           </nav>
         </div>
+        <SeriesAdWide
+          primary={'from-red-500'}
+          secondary={'to-orange-400'}
+          btnColor={'bg-red-600'}
+          imageUrl="https://s2-techtudo.glbimg.com/jT_Nq_aLiobRkV4z782uiWlZ2UY=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/J/8/EbfA0BTv2pV98gASWdfA/f5d0f04e85a224493fe9caa2e59740811630901421-main.png"
+        />
 
         <div className="max-w-full w-full">
           <div className="mt-10">
@@ -49,15 +64,25 @@ export function SeriesNavigator() {
             </p>
             <Separator />
           </div>
-
-          <nav className="h-[40rem] max-w-full w-full">
+          <nav className="h-[30rem] max-w-full w-full">
             <SeriesCard />
           </nav>
         </div>
       </div>
 
-      <div className="my-20 w-full bg-gradient-to-t from-starBlue-400 to-cyan-300 flex justify-center px-8 sm:px-0">
+      <div className="mb-32 w-full bg-gradient-to-t from-starBlue-400 to-cyan-300 flex justify-center sm:p-4">
         <img src="/src/assets/horimiyaPoster.png" alt="" />
+      </div>
+
+      <div className="self-center mx-auto w-[19rem] p-8">
+        <h2 className="text-lg font-medium text-zinc-300">
+          Você chegou ao fim da lista...
+        </h2>
+        <img
+          src="/src/assets/yuzu.png"
+          className="w-40 ml-9 mt-8 mb-20 self-center h-full"
+          alt=""
+        />
       </div>
     </main>
   );
